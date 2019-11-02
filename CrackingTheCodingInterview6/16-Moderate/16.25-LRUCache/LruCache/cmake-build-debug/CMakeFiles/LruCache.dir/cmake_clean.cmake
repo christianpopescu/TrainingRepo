60a46465fbb87d8d007f39cfc5ea502b9d8ecfdd
Christian Popescu <christian.popescu@outlook.com>
2019-11-03 00:37:46 +0100

    change gitignore

---
+++ CrackingTheCodingInterview6/16-Moderate/16.25-LRUCache/LruCache/cmake-build-debug/CMakeFiles/LruCache.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LruCache.dir/main.cpp.o"
+  "LruCache"
+  "LruCache.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LruCache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CrackingTheCodingInterview6/16-Moderate/16.25-LRUCache/LruCache/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+class LinkedNode {
+    int key;
+    std::string value;
+    LinkedNode* prev;
+    LinkedNode* next;
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
