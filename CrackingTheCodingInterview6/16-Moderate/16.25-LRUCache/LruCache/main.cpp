33be516604ddbe7e4b6c003dddaa33a5094aa467
Christian Popescu <christian.popescu@outlook.com>
2019-11-03 10:44:38 +0100

    LRU cache add some logic

--- CrackingTheCodingInterview6/16-Moderate/16.25-LRUCache/LruCache/main.cpp
+++ CrackingTheCodingInterview6/16-Moderate/16.25-LRUCache/LruCache/main.cpp
@@ -2,11 +2,61 @@
 #include <bits/stdc++.h>
 
 class LinkedNode {
+public:
     int key;
     std::string value;
     LinkedNode* prev;
     LinkedNode* next;
 };
+
+class LruCache {
+protected:
+    int maxCash;
+    std::unordered_map<int , LinkedNode* > mapCache;
+    LinkedNode *head = nullptr;
+    LinkedNode *tail = nullptr;
+
+// RemoveNodeFromList
+// ------------------
+    void RemoveNodeFromList(LinkedNode* pNode) {
+        if (pNode == nullptr) return;
+        if (pNode == head) {
+            if (pNode == tail) {
+                // only one node in linked list
+                head = tail = nullptr;
+            }
+            else {
+                head = pNode->next;
+            }
+        }
+        else if (pNode == tail) {
+            tail = pNode->prev;
+        } else {
+            pNode->prev->next = pNode->next;
+            pNode->next->prev = pNode->prev;
+        }
+    }
+// AddNodeInFrontOfList
+// --------------------
+    void AddNodeInFrontOflist(LinkedNode* pNode) {
+        if (head  == nullptr && tail == nullptr) {
+            // empty list
+            head = tail = pNode;
+            tail->prev = nullptr;
+            tail->next = nullptr;
+        }
+        else
+        {
+            pNode->prev = nullptr;
+            pNode->next = head->next;
+            head = pNode;
+        }
+
+    }
+
+};
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
