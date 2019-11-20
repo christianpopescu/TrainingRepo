31dae8c6728f0764c56265c8a84d9243b2e96fe5
Christian Popescu <christian.popescu@outlook.com>
2019-11-21 00:11:16 +0100

    Eight queens with 92 solutions

--- DataStructuresAndAlgorithms/Backtraking/EightQueens.cpp
+++ DataStructuresAndAlgorithms/Backtraking/EightQueens.cpp
@@ -3,6 +3,7 @@
 
 #define N 8
 using namespace std;
+int nbSolutions = 0;
 std::vector<int> solution(N + 1, 0);
 
 void visit(const std::vector<int>& sol) {
@@ -10,15 +11,16 @@ void visit(const std::vector<int>& sol) {
     std :: cout << "(" << i << ", " << sol[i] << ")  ";    
 } 
   std::cout << std::endl;
+  ++nbSolutions;
 }
 
 // Class that keeps the "covered" areas by Queens
 class OccupiedCells{
 public:
   OccupiedCells() : 
-    column(N,false),
-    diag1(2*N-1,false),
-    diag2(2*N-1,false)  {
+    column(N+1,false),
+    diag1(2*N-1+1,false),
+    diag2(2*N-1+1,false)  {
   }
 public:
   bool trySet(int line, int col) {
@@ -26,18 +28,21 @@ public:
     //    if possible - block the corresponding column and diags
     //       returns true;
     //    else - returns false;
+    //    cout << line << "  " << col << "   "<< column[col]<< endl;
     if (column[col] ) return false;
-    int d1 = col - line + 3 ;
+    int d1 = col - line + N-1 ;
     int d2 = col + line - 2;
+    // cout<< d1 << "  " << d2 << endl;
     if (diag1[d1] ) return false;
     if (diag2[d2] ) return false;
+    //     cout<< d1 << "  " << d2 << endl;
     column[col] = true;
     diag1[d1] = true;
     diag2[d2] = true;
     return true;
   }
   bool unSet(int line, int col) {
-    int d1 = col - line + 3 ;
+    int d1 = col - line + N - 1 ;
     int d2 = col + line - 2;
     column[col] = false;
     diag1[d1] = false;
@@ -51,10 +56,13 @@ private :
 
 };
 
+OccupiedCells oc;
+
 void Backtrack(int k) {
-  cout << k << endl;
-  OccupiedCells oc;
+  //  cout << k << endl;
+  
   for (int i=1; i<= N; ++i) {
+    //    cout << " - " << i << endl;
     if (oc.trySet(k, i) ) {
           solution[k] = i;
           if (k == N) {
@@ -62,8 +70,10 @@ void Backtrack(int k) {
 	  }
 	  else  {
 	    Backtrack(k+1);
-	    solution[k] = i;
-	}
+	   
+	  }
+	  solution[k] = 0;
+	  oc.unSet(k, i);
     }
   }
 }
@@ -71,6 +81,7 @@ void Backtrack(int k) {
 int main(){
   Backtrack(1);
   
+  std::cout << "Solutions = "<< nbSolutions << std::endl;
   std::cout << "End" << std::endl;
   
 }
