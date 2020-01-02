3a2e24a75c699f825ac5e1ea6a64764e7894b7a2
Christian Popescu <christian.popescu@outlook.com>
2020-01-02 22:46:04 +0100

    power set combinatorics

--- CrackingTheCodingInterview6/08-RecursionAndDynamicProgramming/P8.04.Powerset.cc
+++ CrackingTheCodingInterview6/08-RecursionAndDynamicProgramming/P8.04.Powerset.cc
@@ -16,27 +16,45 @@ public:
 	int count = 0;
 	int magic_index = -1;
 	
+	vector<int> ConvertIntToSet(int k, vector<int>& set) {
+		vector<int> subset;
+		int index = 0;
+		for (int i = k; i>0; i>>=1) {
+			if (i & 1 == 1) {
+				subset.push_back(set[index]);
+			}
+			index++;
+		}
+		return subset;
+	}
+	
 	// Combinatorics version
 	// generates all the numbers from 1 to 2^n. For each number use the bit as index of set.
-	vector<vector<int>> all generateAllSubsets(vector<int> set) {
+	vector<vector<int>> generateAllSubsets(vector<int>& set) {
 	   int max = 1 << set.size();    // Computes 2^name
-	   
-	   
+	   vector<vector<int>> result;
+	   for (int i = 0; i < max; ++i) {
+		   result.push_back(ConvertIntToSet(i, set));
+	   }
+	   return result;
+	}
+	
+	void PrintAllSubsets(vector<vector<int>>& subsets) {
+		cout << "Number of subsets " << subsets.size() << endl;
+		for (auto ss : subsets) {
+			for (auto s : ss) {
+				cout << s << " " ;
+			}
+			cout << endl;
+		}
 	}
 };
 
 int main(){
-    MagicIndex mi;
-	mi.array.push_back(-10);
-	mi.array.push_back(-5);
-	mi.array.push_back(-3);
-	mi.array.push_back(0);
-	mi.array.push_back(4);
-	mi.array.push_back(5);
-	mi.array.push_back(6);
-	mi.array.push_back(8);
-	for (int i : mi.getMagicIndex())
-		cout << i << endl;
+	Powerset ps;
+	vector<int> set = {7, 8, 9};
+	auto subsets = ps.generateAllSubsets(set);
+	ps.PrintAllSubsets(subsets);
 	cout << "End of problem";
 	
 	return 0;
