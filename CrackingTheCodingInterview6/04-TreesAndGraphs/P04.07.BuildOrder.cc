97697b3cba31d4bb6103a9cf4d7dbb283cba4212
Christian Popescu <christian.popescu@outlook.com>
2020-01-14 23:44:49 +0100

    /P04.07.BuildOrder.exe dfs version ok

--- CrackingTheCodingInterview6/04-TreesAndGraphs/P04.07.BuildOrder.cc
+++ CrackingTheCodingInterview6/04-TreesAndGraphs/P04.07.BuildOrder.cc
@@ -13,7 +13,7 @@ using namespace std;
 
 class GraphNode {
 	public:
-	GraphNode(char _key):key{key} {};
+	GraphNode(char _key):key{_key} {};
 	vector<int> children;
 	char key;
 };
@@ -28,6 +28,8 @@ class Graph{
 	vector<GraphNode> nodes;
 	vector<visit> visited_status;
 	unordered_map<char,int>index;
+	deque<int> sorted_list;
+	
 	Graph& addNode(char c) {
 		nodes.push_back(GraphNode(c));
 		visited_status.push_back(visit::not_visited);
@@ -37,6 +39,30 @@ class Graph{
 	Graph& addEdge(char u, char v) {
 		nodes[index[u]].children.push_back(index[v]);
 		return *this;
+	}
+	
+	bool dfs(int node, deque<int>& sorted_list) {
+		visited_status[node] = visit::visiting;
+		for (auto n : nodes[node].children) {
+			if (visited_status[n] == visit::visiting) return false;
+			if (visited_status[n] == visit::visited) continue;
+			bool b = dfs(n, sorted_list);
+			if (!b) return false;				
+		}
+		visited_status[node] = visit::visited;
+		sorted_list.push_front(node);
+		return true;
+	}
+	
+	bool TopologicalSort() {
+		bool b = true;
+		for (int i = 0; i < nodes.size(); ++i ) {
+			if (visited_status[i] == visit::not_visited) {
+				b = dfs(i, sorted_list);
+				if (!b) return false;
+			}
+		}
+		return true;
 	}
 
 };
@@ -48,6 +74,13 @@ int main(){
 	Graph gs;
 	gs.addNode('a').addNode('b').addNode('c').addNode('d').addNode('e').addNode('f');
 	gs.addEdge('a','b').addEdge('f','b').addEdge('b','d').addEdge('f','a').addEdge('d','c');
-
+    if (gs.TopologicalSort()) {
+		for (auto n : gs.sorted_list){
+			cout << gs.nodes[n].key << ' ';
+		}
+		cout << endl;
+	} else {
+		cout << "not possible to Topological Sort the given graph" << endl;
+	}
 	return 0;
 }
