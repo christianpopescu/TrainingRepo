4f5ee51b07c80454827f6a9a65e361eef3549ac3
Christian Popescu <christian.popescu@outlook.com>
2020-04-02 11:36:13 +0200

    Init Chapter 02  with problem 01 remove duplicates

---
+++ CrackingTheCodingInterview6/02-LinkedList/p02.01.RemoveDuplicates.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class ListNode {
+	public:
+	
+	ListNode* next;
+	int data;
+};
+
+class SimpleLinkedList {
+	public:
+	SimpleLinkedList() : head{nullptr} {};
+	ListNode* head;
+};
+
+int main() {
+	
+	
+	return 0;
+}
